69c582832018686dbdc243c83b0d08e037128b86
Leandro <kiyohiroleo@gmail.com>
2026-09-14 20:48:49 -0300

    pascalCase para snake_case

--- unittest_vowels.py
+++ unittest_vowels.py
@@ -4,26 +4,26 @@ from count_utils import count_vowels
 class CountVowels(unittest.TestCase):
 
     #Teste com uma String vazia.
-    def testEmptyString(self):
+    def test_empty_string(self):
         self.assertEqual(count_vowels(""), 0)
     #Teste usando um valor numérico como entrada, caso a entrada cause um TypeError, está correto.
-    def testNumber(self):
+    def test_number(self):
         with self.assertRaises(TypeError):
             count_vowels(0)
     #Teste se não há vogais.
-    def testNoVowels(self):
+    def test_no_vowels(self):
         self.assertEqual(count_vowels("wxyz"), 0)
 
     # Teste se símbolos são ignorados sem interferir na contagem das vogais.
-    def testSymbols(self):
+    def test_symbols(self):
         self.assertEqual(count_vowels("a!e@i#o$u%"), 5)
 
     # Teste com letras maiúsculas e minúsculas.
-    def testUpperAndLowerCase(self):
+    def test_upper_lower_case(self):
         self.assertEqual(count_vowels("OpenAI"), 4)
 
     # Teste com uma frase próxima de uma entrada real da aplicação.
-    def testSentence(self):
+    def test_sentence(self):
         self.assertEqual(count_vowels("Teste e Validacao de Sistemas!"), 12)
 
 if __name__ == '__main__':
