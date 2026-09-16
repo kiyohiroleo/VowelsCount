a812e1dccc067de337deaecdbb54f0aef48e5997
Leandro <kiyohiroleo@gmail.com>
2026-09-16 11:16:57 -0300

    comentário em count_utils

--- count_utils.py
+++ count_utils.py
@@ -1,4 +1,7 @@
 def count_vowels(s):
+    #Checa se é uma string.
     if not isinstance(s, str):
+        #TypeError, caso não seja.
         raise TypeError("Expected a String")
+    #Transforma a string em letras minúsculas e checa se as vogais está presente.
     return sum(1 for c in s.lower() if c in "aeiou")
